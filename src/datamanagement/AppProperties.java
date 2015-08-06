34081f516d8426bd916625b2c198d0edce20ca1b
Camo-flage <camo_brierley@hotmail.com>
2015-08-07 02:31:12 +1000

    Formatting

--- src/datamanagement/AppProperties.java
+++ src/datamanagement/AppProperties.java
@@ -11,14 +11,18 @@ public class AppProperties {
 		if (self == null) {
 			self = new AppProperties();
 		}
+		
 		return self;
 	}
 
 	private AppProperties() {
 		properties = new Properties();
+		
 		try {
 			properties.load(new FileInputStream("Properties.prop"));
-		} catch (IOException e) {
+		}
+		
+		catch (IOException e) {
 			throw new RuntimeException("Could not read property file");
 		}
 	}
