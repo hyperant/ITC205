29277c6797c69f9f5a8c70bae61f1cdcb436e77f
hyperant <toja.design@gmail.com>
2015-08-09 11:09:30 +1000

    added in java doc comments to all functions except for the last one, need to look into a possible bug in this function.
    Signed-off-by: hyperant <toja.design@gmail.com>

--- src/datamanagement/cgCTL.java
+++ src/datamanagement/cgCTL.java
@@ -9,23 +9,30 @@ package datamanagement;
 public class cgCTL {
 
 	private cgUI CGUI;
-	private String cuc = null;
+	private String unitCode = null;
 	private Integer currentStudentID = null;
 	private boolean changed = false;
 
 	/**
-	 * The construtore method for the class. This class accepts no input, and only creates a new instance of this class.
+	 * The constructor method for the class. This class accepts no input, and only creates a new instance of this class.
 	 * </br></br>
-	 * In order to display the GUI to the end user you must call .execute()
+	 * In order to display the GUI to the end user you must call {@link #execute()}
 	 * </br>
-	 * Example:</br>
-	 * <code>
-	 * new cgCTL().execute();
-	 * </code>
+	 * <b>Example:</b>
+	 * </br>
+	 * <pre>
+	 * {@code
+	 * 	new cgCTL().execute();
+	 * }
+	 * </pre>
 	 */
 	public cgCTL() {
+		
 	}
 
+	/**
+	 * Setups and displays the GUI to the end user
+	 */
 	public void execute() {
 		ListUnitsCTL luCTL = new ListUnitsCTL();
 		
@@ -44,32 +51,42 @@ public class cgCTL {
 		this.CGUI.setState1(true);
 	}
 
-	public void unitSelected(String code) {
-		if (code.equals("NONE")) {
+	
+	/**
+	 * Sets the selected course unit and updates the GUI accordingly.
+	 * 
+	 * @param unitCode The current courses unit code
+	 */
+	public void unitSelected(String unitCode) {
+		if (unitCode.equals("NONE")) { //If we don't have a course unit set then we need to disable the student selection box
 			this.CGUI.setState2(false);
-		} else {
+		} else { //We have a course selected so display the students for that course
 			ListStudentsCTL lsCTL = new ListStudentsCTL();
-			
-			lsCTL.listStudents(this.CGUI, code);
-			this.cuc = code;
+			lsCTL.listStudents(this.CGUI, unitCode);
+			this.unitCode = unitCode;
 			this.CGUI.setState2(true);
 		}
 		
 		this.CGUI.setState3(false);
 	}
 
+	/**
+	 * Sets the selected student and updates the GUI accordingly.
+	 * 
+	 * @param sID The current Students ID
+	 */
 	public void studentSelected(Integer sID) {
 		this.currentStudentID = sID;
 		
-		if (this.currentStudentID.intValue() == 0) {
+		if (this.currentStudentID.intValue() == 0) { //If we don't have a student set then we need to disable the assignment mark box's
 			this.CGUI.Refresh3();
 			this.CGUI.setState3(false);
 			this.CGUI.setState4(false);
 			this.CGUI.setState5(false);
 			this.CGUI.setState6(false);
-		} else {
+		} else { //We have a student selected so display the assignment mark box's
 			IStudent studentmManager = StudentManager.get().getStudent(sID);
-			IStudentUnitRecord studentRecord = studentmManager.getUnitRecord(this.cuc);
+			IStudentUnitRecord studentRecord = studentmManager.getUnitRecord(this.unitCode);
 
 			this.CGUI.setRecord(studentRecord);
 			this.CGUI.setState3(true);
@@ -80,9 +97,17 @@ public class cgCTL {
 		}
 	}
 
-	public String checkGrade(float f, float g, float h) {
-		IUnit unit = UnitManager.UM().getUnit(this.cuc);
-		String grade = unit.getGrade(f, g, h);
+	/**
+	 * Checks the current students grade
+	 * 
+	 * @param mark1 the students mark for their first assignment
+	 * @param mark2 the students mark for their second assignment
+	 * @param mark3 the students mark for their third assignment
+	 * @return {@link #String} representation of the current students grade
+	 */
+	public String checkGrade(float mark1, float mark2, float mark3) {
+		IUnit unit = UnitManager.UM().getUnit(this.unitCode);
+		String grade = unit.getGrade(mark1, mark2, mark3);
 		
 		this.CGUI.setState4(true);
 		this.CGUI.setState5(false);
@@ -94,6 +119,9 @@ public class cgCTL {
 		return grade;
 	}
 
+	/**
+	 * Allow the students marks to be changed
+	 */
 	public void enableChangeMarks() {
 		this.CGUI.setState4(false);
 		this.CGUI.setState6(false);
@@ -102,10 +130,12 @@ public class cgCTL {
 	}
 
 	public void saveGrade(float asg1, float asg2, float exam) {
-		IUnit unit = UnitManager.UM().getUnit(this.cuc);
+		//possible bug? unit is not used
+		
+		IUnit unit = UnitManager.UM().getUnit(this.unitCode);
 		IStudent studentManager = StudentManager.get().getStudent(this.currentStudentID);
 		
-		IStudentUnitRecord r = studentManager.getUnitRecord(this.cuc);
+		IStudentUnitRecord r = studentManager.getUnitRecord(this.unitCode);
 		r.setAsg1(asg1);
 		r.setAsg2(asg2);
 		r.setExam(exam);
