7dc7df27b1521af972b13e97864ece9539738e41
Camo-flage <camo_brierley@hotmail.com>
2015-08-11 14:03:44 +1000

    Formatting, automatically added final serialVersionUID

--- src/datamanagement/StudentMap.java
+++ src/datamanagement/StudentMap.java
@@ -1,2 +1,7 @@
 package datamanagement;
-public class StudentMap extends java.util.HashMap<Integer, IStudent> {}
+
+public class StudentMap extends java.util.HashMap<Integer, IStudent> {
+
+	private static final long serialVersionUID = 1L;
+
+}
