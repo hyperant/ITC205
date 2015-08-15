968330899945de1f245f1e3af5e4f569d926a7e8
hyperant <toja.design@gmail.com>
2015-08-15 11:03:13 +1000

    added comments
    Signed-off-by: hyperant <toja.design@gmail.com>

--- src/datamanagement/ListStudentsCTL.java
+++ src/datamanagement/ListStudentsCTL.java
@@ -3,10 +3,21 @@ package datamanagement;
 public class ListStudentsCTL {
 	private StudentManager studentManager;
 
+	/**
+	 * The constructor method for the class.
+	 * <br />
+	 * Initilizes studentManager
+	 */
 	public ListStudentsCTL() {
 		this.studentManager = StudentManager.get();
 	}
 
+	/**
+	 * Clears and adds students to the studentLister object for a specified subject
+	 * 
+	 * @param lister the object to add the students to
+	 * @param unitCode subject code to list the students from
+	 */
 	public void listStudents(IStudentLister lister, String unitCode) {
 		lister.clearStudents();
 		StudentMap students = this.studentManager.getStudentsByUnit(unitCode);
